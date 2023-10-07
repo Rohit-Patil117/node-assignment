ab921a60a979c55c964f30acbe2b13332f34bbd5
Rohit Patil <rp.rohitpatil117@gmail.com>
2023-10-07 18:27:40 +0530

    Added input validation

--- src/ride/input/ride-request.input.ts
+++ src/ride/input/ride-request.input.ts
@@ -1,10 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class RideRequestInput {
   @Field(() => String)
+  @IsNotEmpty()
   pickupLocation: string;
 
   @Field(() => String)
+  @IsNotEmpty()
   droppingLocation: string;
 }
--- src/user/input/register-user.input.ts
+++ src/user/input/register-user.input.ts
@@ -1,16 +1,26 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, Matches, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class RegisterUserInput {
   @Field(() => String)
+  @IsNotEmpty()
   name: string;
 
   @Field(() => String)
+  @IsNotEmpty()
+  @IsEmail({}, { message: 'Invalid email address' })
   email: string;
 
   @Field(() => String)
+  @IsNotEmpty()
+  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/, {
+    message:
+      'Password must contain at least eight characters, one number, one lower and uppercase letter and one special character',
+  })
   password: string;
 
   @Field(() => String)
+  @IsNotEmpty()
   address: string;
 }
